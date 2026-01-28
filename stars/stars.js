9f4a8ae85af3ddddf107a41416580e09ceb01b6b
ForrestGump618 <2583990522@qq.com>
2026-01-28 21:35:50 +0800

    Site updated: 2026-01-28 21:35:50

--- stars/stars.js
+++ stars/stars.js
@@ -1,66 +1,34 @@
-const stars = {
-  init(url, containerSelector = 'body') {
-    const that = this;
-    $.getJSON(url)
-      .done(function(data) {
-        for (const className in data) {
-          if (!Object.prototype.hasOwnProperty.call(data, className)) continue;
-          const classData = data[className];
-          that.render(classData, className, containerSelector);
+// 采用一个多对象JSON文件存储所有数据的方式
+var stars = {
+    init: function(url) {
+        var that = this;
+        $.getJSON(url, function(data) {
+            for (var className in data) {
+                var classData = data[className];
+                that.render(classData, className);
+            }
+        });
+    },
+
+    render: function(data, name) {
+        var nickname, site, content, li = "";
+        for (var i = 0; i < data.length; i++) {
+            nickname = data[i].nickname;
+            site = data[i].site;
+            content = data[i].content;
+
+            li += '<div class="card" onclick="window.open(\'' + site + '\')">' +
+                      '<div class="card-header">' +
+                          '<div>' + nickname + '</div>' +
+                      '</div>' +
+                      '<div class="card-content">' +
+                          '<div>' + content + '</div>' +
+                      '</div>' +
+                  '</div>';
         }
-      })
-      .fail(function(jqxhr, textStatus, error) {
-        console.error('Failed to load JSON:', textStatus, error);
-      });
-  },
-
-  render(data, name, containerSelector = 'body') {
-    if (!Array.isArray(data) || data.length === 0) return;
-    const $container = $(containerSelector);
-    if ($container.length === 0) {
-      console.warn('Container not found:', containerSelector);
-      return;
+        $(name).append(li);
     }
+}
 
-    // 构造 HTML 字符串，最后一次性 append
-    const html = data.map(item => {
-      const nickname = this.escapeHtml(item.nickname || '');
-      const site = item.site || '';
-      const content = this.escapeHtml(item.content || '');
-      // 使用 data-site 存储链接，避免内联 onclick
-      return `<div class="card" data-site="${this.escapeAttr(site)}">
-                <div class="card-header"><div>${nickname}</div></div>
-                <div class="card-content"><div>${content}</div></div>
-              </div>`;
-    }).join('');
-
-    $container.append(html);
-
-    // 通过事件委托处理点击（只绑定一次）
-    if (!$container.data('stars-click-bound')) {
-      $container.on('click', '.card', function() {
-        const url = $(this).data('site');
-        if (url) window.open(url, '_blank');
-      });
-      $container.data('stars-click-bound', true);
-    }
-  },
-
-  // 简单文本转义
-  escapeHtml(str) {
-    return String(str).replace(/[&<>"']/g, s => ({
-      '&': '&amp;',
-      '<': '&lt;', '>': '&gt;',
-      '"': '&quot;',
-      "'": '&#39;'
-    }[s]));
-  },
-
-  // 属性值中简单转义双引号
-  escapeAttr(str) {
-    return String(str).replace(/"/g, '&quot;');
-  }
-};
-
-// 调用示例（假设页面上有 <div id="stars"></div>）
-// stars.init('./allStars.json', '#stars');</':>
+// 传入json文件的路径
+stars.init("./allStars.json");
