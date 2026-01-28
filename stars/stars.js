ab7156e4a92e9bfd039801ef5ca6bd1ffce793ec
ForrestGump618 <2583990522@qq.com>
2026-01-28 18:32:33 +0800

    Site updated: 2026-01-28 18:32:33

--- stars/stars.js
+++ stars/stars.js
@@ -1,30 +1,29 @@
-// 采用一个多对象JSON文件存储所有数据的方式
-stars = {
+var stars = {
   init: function(url) {
     var that = this;
-    $.getJSON(url,
-      function(data){
+    $(function() {  // DOM ready
+      $.getJSON(url, function(data){
         for(var className in data){
-          var classData = data[className]
-          that.render(classData,className)
+          var classData = data[className];
+          that.render(classData, className);
         }
-      }
-    )
+      });
+    });
   },
   render: function(data, name) {
-      var nickname, site, li = "";
-      for (var i = 0; i < data.length; i++) {
-          nickname = data[i].nickname;
-          site = data[i].site;
-          content = data[i].content;
-          li += '<div class="card" onclick="window.open(\'' + site +'\')">' + '<div class="card-header">' + '<div>' + nickname + '</div>' + '</div>' + '<div class="card-content">' + '<div>' + content + '</div>' + '</div>' + '</div>';
-      }
-        $(name).append(li);
+    var nickname, site, content, li = "";
+    for (var i = 0; i < data.length; i++) {
+      nickname = data[i].nickname;
+      site = data[i].site;
+      content = data[i].content;
+      li += '<div class="card" onclick="window.open(\'' + site +'\')">' +
+              '<div class="card-header"><div>' + nickname + '</div></div>' +
+              '<div class="card-content"><div>' + content + '</div></div>' +
+            '</div>';
+    }
+    $('.' + name).append(li);  // 加上 . 选择器
   }
 }
-// 传入json文件的路径
-stars.init("./allStars.json")
 
-
-
-
+// 调用初始化
+stars.init("./allStars.json");
