d46d325079bb613b0f2d992b79a3010571fc8caa
結露 <68598198+XketsuroX@users.noreply.github.com>
2026-05-11 22:01:22 +0800

    feat: support remote URLs and local JSON source for header carousel

--- src/toolkit/themeConfig.defaults.ts
+++ src/toolkit/themeConfig.defaults.ts
@@ -67,6 +67,8 @@ export const DEFAULT_THEME_CONFIG: ShokaXThemeConfig = {
       enable: true,
       url: "cover-4",
     },
+    carouselUrls: [],
+    carouselSource: "",
     nextGradientCover: false,
   },
   sidebar: {
